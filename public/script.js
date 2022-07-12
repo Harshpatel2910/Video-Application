8caa361a739c112104706005dbbaab9d29e40c39
Harshpatel2910 <hppatel530@gmail.com>
2022-07-12 10:57:03 +0530

    change script

--- public/script.js
+++ public/script.js
@@ -1,6 +1,11 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 
+// const myPeer = new Peer(undefined, {
+//   host: '/',
+//   port: '3001'
+
+// })
 
 const myPeer = new Peer()
 const myVideo = document.createElement('video')
